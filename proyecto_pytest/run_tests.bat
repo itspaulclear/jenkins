a8390d68034522790c72c3362b5f00e4510f3371
Pablo García Rodríguez <pgarciaro@ayesa.es>
2025-04-23 13:18:37 +0200

    correccion de entorno virtual

--- proyecto_pytest/run_tests.bat
+++ proyecto_pytest/run_tests.bat
@@ -1,8 +1,13 @@
 @echo off
 echo "activando el entorno virtual"
+if[!"-d venv" ]; then
+    python -m venv venv
+fi
+
 call venv\Scripts\activate
 
 echo "instalando dependencias"
+pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "ejecutando pruebas con pytest"
